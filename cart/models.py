e8c85a99ddbbba83073416e4b64a233693c329f6
Dewald Laurens <dewaldlaurens@Dewald-MacBook-Pro.local>
2018-10-12 15:19:49 +0200

    Added some functionality to cart and browse page

--- cart/models.py
+++ cart/models.py
@@ -1,11 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
-from django.auth import User
 
-from Store.models import StoreItem
+from stores.models import StoreItem
 
 
 class PendingTransaction(models.Model):
     user_id = models.ForeignKey(User, on_delete='CASCADE')
     store_item_id = models.ForeignKey(StoreItem, on_delete='CASCADE')
     quantity = models.IntegerField()
-    # TODO: finish this model
--- stores/views.py
+++ stores/views.py
@@ -1,11 +1,22 @@
+from pprint import PrettyPrinter
+
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from cart.models import PendingTransaction
 from stores.models import Item, StoreItem
-from pprint import PrettyPrinter
 
 
 def store_items(request, store_id):
     # inventory = StoreItem.objects.filter(store_id=store_id)
-    inventory = StoreItem.objects.select_related('item_id').filter(store_id=store_id)    
-    context = {'inventory': inventory}
+    inventory = StoreItem.objects.prefetch_related(
+        'item_id').filter(store_id=store_id)
+
+    # cart = PendingTransaction.objects.select_related('user_id')
+    cart = PendingTransaction.objects.prefetch_related('user_id')
+    print(str(cart.query))
+    context = {
+        'inventory': inventory,
+        'cart': cart
+    }
     return render(request, 'stores/store_items.html', context)
